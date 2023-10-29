606fd73f87581b2929b05624938fc1be9baf42b2
Munoko19 <19munoko@gmail.com>
2023-10-29 21:20:13 +0700

    Uji Ketrampilan

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-/*
-    Tugas praktek sesi 5: 
-    1. buat table task dengan kolom : 
-    - task_id (int)
-    - user_id (int)
-    - task_name (varchar)
-    - task_descriptioin (varchar)
-    - is_done (int)
-
-    2. Buat Endpoint untuk Create, Read, Update, Delete (CRUD) operation untuk table tersebut.
-        - untuk function create user_id di isi berdasarkan id user yang terauthrosisasi
-    3. Tambahkan authorization untuk setiap endpointnya.
-*/
 import * as UserService from "./services/user.js";
 import express from "express";
 
